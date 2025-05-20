5d3a0501557aa27af15350139f39b95c6c928960
capea6 <capea6@ext.uv.es>
2025-05-20 11:39:13 +0200

    Update CheckFiles.bat

--- CheckFiles.bat
+++ CheckFiles.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-curl "https://raw.githubusercontent.com/mon5termatt/medicat_installer/main/hasher/MedicatFiles.md5" -o  ./MedicatFiles.md5 -s -L
+curl "https://raw.githubusercontent.com/capea6/Medicat-UV/refs/heads/main/MedicatFiles.md5" -o  ./MedicatFiles.md5 -s -L
 echo.Now Checking files.
 start QuickSFV MedicatFiles.md5
 del %0 && exit
